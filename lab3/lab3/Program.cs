b98ffe8564a5f2aa4b5bc1b56a3513a5f7309859
Nick Filon <132257079+nFeelon@users.noreply.github.com>
2023-10-19 12:24:13 +0300

    Add files via upload

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+String str1 = new String("ABCD! Efgd, 124.");
+String str2 = new String("5124? Sada. B23ab.");
+
+Console.WriteLine(str1.Length());
+Console.WriteLine(str2.Length());
+
+Console.WriteLine(str1 > str2);
+Console.WriteLine(str1 < str2);
+Console.WriteLine(str1 == str2);
+Console.WriteLine(str1 != str2);
+
+Console.WriteLine((str1+25).Value);
+Console.WriteLine((str2 * 'a').Value); ;
+Console.WriteLine((-str1).Value);
+
+Console.WriteLine(str1.HasControlChars());
+Console.WriteLine((str2.RemovePunct()).Value);
+
+String.Production p = new String.Production("Microsoft", 0);
+String.Developer d = new String.Developer("Nikita Filon", 1, "GameDev");
+String obj = new String("Hello", "Valve", 1, "Neapl", 2, "GameDev");
+obj.ShowInfo();
+
+Console.WriteLine((str1.Sum(str2)).Value);
+Console.WriteLine(str1.Diff());
+Console.WriteLine(str1.Count());
