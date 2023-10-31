bf810f621b22c5d8c2cc9eacd9b43614fde0ab35
Nikita Filon <132257079+nFeelon@users.noreply.github.com>
2023-10-31 15:53:26 +0300

    Add files via upload

---
+++ lab8/lab8/Events.cs
@@ -0,0 +1,93 @@
+﻿public class Boss
+{
+    public delegate void UpgradeEventHandler(object sender, EventArgs e);
+    public delegate void TurnOnEventHandler(object sender, TurnOnEventArgs e);
+
+    public event UpgradeEventHandler Upgrade;
+    public event TurnOnEventHandler TurnOn;
+
+    public void RaiseUpgrade()
+    {
+        Console.WriteLine("Boss: Я повышаю уровень!");
+        if (Upgrade != null)
+        {
+            Upgrade(this, EventArgs.Empty);
+        }
+    }
+
+    public void RaiseTurnOn(int voltage)
+    {
+        Console.WriteLine($"Boss: Я включаюсь под напряжением {voltage} В!");
+        if (TurnOn != null)
+        {
+            TurnOnEventArgs args = new TurnOnEventArgs(voltage);
+            TurnOn(this, args);
+        }
+    }
+
+}
+
+public class TurnOnEventArgs : EventArgs
+{
+    public int Voltage { get; set; }
+    public TurnOnEventArgs(int voltage)
+    {
+        Voltage = voltage;
+    }
+
+}
+
+public class Robot
+{
+    public string Name { get; set; }
+
+    public Robot(string name)
+    {
+        Name = name;
+    }
+
+    public void OnUpgrade(object sender, EventArgs e)
+    {
+        Console.WriteLine($"{Name}: Я получил новые возможности!");
+    }
+
+    public void OnTurnOn(object sender, TurnOnEventArgs e)
+    {
+        if (e.Voltage > 1000)
+        {
+            Console.WriteLine($"{Name}: Я сломался из-за перенапряжения!");
+        }
+        else
+        {
+            Console.WriteLine($"{Name}: Я успешно включился!");
+        }
+    }
+
+}
+
+public class Cyborg
+{
+    public string Name { get; set; }
+    public Cyborg(string name)
+    {
+        Name = name;
+    }
+
+    public void OnUpgrade(object sender, EventArgs e)
+    {
+        Console.WriteLine($"{Name}: Я стал более человечным!");
+    }
+
+    public void OnTurnOn(object sender, TurnOnEventArgs e)
+    {
+        if (e.Voltage > 500)
+        {
+            Console.WriteLine($"{Name}: Я пострадал из-за перенапряжения!");
+        }
+        else
+        {
+            Console.WriteLine($"{Name}: Я успешно включился!");
+        }
+    }
+
+}
---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Boss boss = new Boss();
+        boss.Upgrade += (this, Boss.UpgradeEventHandler upgradeEventHandler) =>
+        {
+            Console.WriteLine("Вы вызвали анонимную функцию!");
+        };
+
+
+        Robot robot1 = new Robot("R2-D2");
+        Robot robot2 = new Robot("C-3PO");
+        Cyborg cyborg1 = new Cyborg("Terminator");
+        Cyborg cyborg2 = new Cyborg("Robocop");
+
+        boss.Upgrade += robot1.OnUpgrade;
+        boss.Upgrade += cyborg2.OnUpgrade;
+        boss.TurnOn += robot2.OnTurnOn;
+        boss.TurnOn += cyborg1.OnTurnOn;
+
+        boss.RaiseUpgrade();
+        Console.WriteLine("\n");
+        boss.RaiseTurnOn(600);
+
+        Console.WriteLine("\nПосле событий:");
+        Console.WriteLine($"Имя робота 1: {robot1.Name}");
+        Console.WriteLine($"Имя робота 2: {robot2.Name}");
+        Console.WriteLine($"Имя киборга 1: {cyborg1.Name}");
+        Console.WriteLine($"Имя киборга 2: {cyborg2.Name}");
+
+        Console.ReadKey();
+
+        string input = "Это пример строки, которая будет обработана разными методами.";
+        Console.WriteLine("Исходная строка: " + input);
+
+        Action<string> removePunctuation = RemovePunctuation;
+        Func<string, string> addSymbols = AddSymbols;
+        Func<string, string> toUpperCase = ToUpperCase;
+        Func<string, string> removeExtraSpaces = RemoveExtraSpaces;
+        Predicate<string> isPalindrome = IsPalindrome;
+
+        removePunctuation(input);
+        input = addSymbols(input);
+        input = toUpperCase(input); 
+        input = removeExtraSpaces(input);
+        bool palindrome = isPalindrome(input); 
+
+        Console.WriteLine("Обработанная строка: " + input);
+        Console.WriteLine("Является ли строка палиндромом: " + palindrome);
+    }
+
+    static void RemovePunctuation(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in s)
+        {
+            if (!char.IsPunctuation(c))
+            {
+                sb.Append(c);
+            }
+        }
+        s = sb.ToString();
+    }
+
+    static string AddSymbols(string s)
+    {
+        return "*" + s + "*";
+    }
+
+    static string ToUpperCase(string s)
+    {
+        return s.ToUpper();
+    }
+
+    static string RemoveExtraSpaces(string s)
+    {
+        return string.Join(" ", s.Split().Where(x => !string.IsNullOrWhiteSpace(x)));
+    }
+
+    static bool IsPalindrome(string s)
+    {
+        int i = 0;
+        int j = s.Length - 1;
+        while (i < j)
+        {
+            if (s[i] != s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
