78610498fccb03208c6ae71018de360287886a8e
Nikita Filon <132257079+nFeelon@users.noreply.github.com>
2023-11-06 18:28:40 +0300

    Add files via upload

---
+++ lab9/lab9/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Furniture
+{
+    public string Name { get; set; }
+    public string Material { get; set; }
+    public double Price { get; set; }
+
+    public Furniture(string name, string material, double price)
+    {
+        Name = name;
+        Material = material;
+        Price = price;
+    }
+
+    public void PrintInfo() => Console.WriteLine("Название: {0}, Материал: {1}, Цена: {2}", Name, Material, Price);
+}
+
+public class FurnitureCollection : IList<Furniture>
+{
+    private ArrayList _list;
+
+    public FurnitureCollection() => _list = new ArrayList();
+    public int Count
+    {
+        get { return _list.Count; }
+    }
+    public bool IsReadOnly
+    {
+        get { return _list.IsReadOnly; }
+    }
+    public Furniture this[int index]
+    {
+        get { return (Furniture)_list[index]; }
+        set { _list[index] = value; }
+    }
+
+    public void Add(Furniture item) => _list.Add(item);
+    public void Clear() => _list.Clear();
+    public bool Contains(Furniture item) => _list.Contains(item);
+    public void CopyTo(Furniture[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
+    public IEnumerator<Furniture> GetEnumerator() => new FurnitureEnumerator(_list);
+    public int IndexOf(Furniture item) => _list.IndexOf(item);
+    public void Insert(int index, Furniture item) => _list.Insert(index, item);
+    public bool Remove(Furniture item)
+    {
+        if (_list.Contains(item))
+        {
+            _list.Remove(item);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void RemoveAt(int index) => _list.RemoveAt(index);
+    IEnumerator IEnumerable.GetEnumerator() => _list.GetEnumerator();
+    private class FurnitureEnumerator : IEnumerator<Furniture>
+    {
+        private ArrayList _list;
+        private int _index;
+        public FurnitureEnumerator(ArrayList list)
+        {
+            _list = list;
+            _index = -1;
+        }
+        public Furniture Current
+        {
+            get { return (Furniture)_list[_index]; }
+        }
+        object IEnumerator.Current
+        {
+            get { return _list[_index]; }
+        }
+        public void Dispose() { }
+        public bool MoveNext()
+        {
+            _index++;
+            return (_index < _list.Count);
+        }
+
+        public void Reset() => _index = -1;
+    }
+}
---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1 задание
+
+        FurnitureCollection fc = new FurnitureCollection();
+        fc.Add(new Furniture("Стол", "Дерево", 5000));
+        fc.Add(new Furniture("Стул", "Пластик", 1000));
+        fc.Add(new Furniture("Диван", "Кожа", 15000));
+        fc.Add(new Furniture("Шкаф", "Металл", 8000));
+        Console.WriteLine("Содержимое коллекции:");
+        foreach (Furniture f in fc)
+        {
+            f.PrintInfo();
+        }
+        fc.RemoveAt(1);
+        fc.Remove(new Furniture("Диван", "Кожа", 15000));
+        Console.WriteLine("Содержимое коллекции после удаления:");
+        foreach (Furniture f in fc)
+        {
+            f.PrintInfo();
+        }
+        Furniture item = new Furniture("Шкаф", "Металл", 8000);
+        if (fc.Contains(item))
+        {
+            Console.WriteLine("Коллекция содержит элемент:");
+            item.PrintInfo();
+        }
+        else
+        {
+            Console.WriteLine("Коллекция не содержит элемент:");
+            item.PrintInfo();
+        }
+        fc.Insert(1, new Furniture("Кресло", "Ткань", 3000));
+        Console.WriteLine("Содержимое коллекции после вставки:");
+        foreach (Furniture f in fc)
+        {
+            f.PrintInfo();
+        }
+
+        //2 задание
+
+        ConcurrentBag<int> nums = new ConcurrentBag<int>();             
+        for (int i = 0; i < 20; i++) nums.Add(i);
+        Console.WriteLine("\nЭлементы первой коллекции до изменения:");
+        foreach (int a in nums)
+            Console.WriteLine(a);
+        int n;
+        for (int i = 0; i < 4; i++)
+        {
+            nums.TryTake(out n); 
+            Console.WriteLine(n);
+        }
+        Console.WriteLine("\nЭлементы первой коллекции после изменения:");
+        foreach (int a in nums)
+            Console.WriteLine(a);
+        List<int> nums2 = new List<int>();
+        while (!nums.IsEmpty)
+        {
+            if (nums.TryTake(out n))
+            {
+                nums2.Add(n);
+            }
+        }
+
+        Console.WriteLine("\nЭлементы второй коллекции:");
+        foreach (int a in nums2)
+            Console.WriteLine(a);
+        if (nums2.Contains(11)) Console.WriteLine("\nnums2 содержит '11'");
+        else Console.WriteLine("\nnums2 не содержит '11'");
+
+        //3 задание
+
+        ObservableCollection<FurnitureCollection> MyCollection = new ObservableCollection<FurnitureCollection>();
+
+        MyCollection.CollectionChanged += MyCollection_onChange;
+
+        MyCollection.Add(fc);
+        MyCollection.RemoveAt(0);
+
+    }
+    private static void MyCollection_onChange(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                Console.WriteLine("Добавлен элемент в коллекцию MyCollection");
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                Console.WriteLine("Удалён элемент в коллекцию MyCollection");
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                Console.WriteLine("Изменен элемент в коллекцию MyCollection");
+                break;
+        }
+    }
+}
